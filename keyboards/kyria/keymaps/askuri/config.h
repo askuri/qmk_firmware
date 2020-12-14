fa6d0288f01672d69a0fa4d39e64f0780f729b74
Martin Weber <enwi2@t-online.de>
2020-12-14 20:47:45 +0100

    Change TT taps to 3
    5, the default, was too much for me. Trying if less is better.

--- keyboards/kyria/keymaps/askuri/config.h
+++ keyboards/kyria/keymaps/askuri/config.h
@@ -30,3 +30,6 @@
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 #define SPLIT_USB_DETECT
 #define NO_USB_STARTUP_CHECK
+
+// 5 taps on TT keys is too much for me
+#define TAPPING_TOGGLE 3
